a370fc5f02babd63e13ef5f573a4805f4ad95f4a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-18 02:06:05 +0530

    setup firestore and fetch data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('hello from index.js')
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBMp46EAkcUxbJL7kHVZohUBkz1jc4WN7E",
+    authDomain: "fir-tutorial-69f22.firebaseapp.com",
+    projectId: "fir-tutorial-69f22",
+    storageBucket: "fir-tutorial-69f22.appspot.com",
+    messagingSenderId: "301336398458",
+    appId: "1:301336398458:web:6480fb917e28c12e2389c3"
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const colRef = collection(db, 'books');
+
+getDocs(colRef).then((snapshot) => {
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach(doc => {
+        books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+}).catch((err) => {
+    console.log(err)
+})
+
